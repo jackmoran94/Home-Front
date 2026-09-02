c7014bb7f2031fdf95e7d5e1b53c64237e1784bb
jackmoran94 <jackmorann94@gmail.com>
2026-09-02 13:36:51 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -93,7 +93,18 @@ function migrate() {
     rota_anchor_date: '2026-09-21', // Day 1 of Template A
     priority_high_days: '14',
     priority_medium_days: '7',
+    priority_low_days: '2',  
+    const defaults = {
+    rota_anchor_date: '2026-09-21', // Day 1 of Template A
+    priority_high_days: '14',
+    priority_medium_days: '7',
     priority_low_days: '2',
+    telegram_bot_token: '',
+    telegram_chat_id: '',
+    telegram_send_time: '07:00',
+    telegram_enabled: '0',
+    telegram_last_sent_date: ''
+  };
     telegram_bot_token: '',
     telegram_chat_id: '',
     telegram_send_time: '07:00',
